5c625a040f433e751c8c55788de12670e4cd375c
mariamaamir22 <160320392+mariamaamir22@users.noreply.github.com>
2024-04-21 18:25:09 +0500

    main.ts

---
+++ main.ts
@@ -0,0 +1,80 @@
+//console.log("hello world");
+
+//let personName="Mariam";
+//console.log(personName.toLowerCase());
+//console.log(personName.toUpperCase());
+//console.log(personName.charAt(0).toUpperCase()+personName.slice(1).toLowerCase());
+
+//Q3
+//console.log("Prophet Muhammad says", "he reward of deeds depend upon the intentions");
+
+//Q5
+//let famousPerson= "Muhammad S A W ";
+//let Message="'said'There is no god but ,Allah";
+//console.log(famousPerson,Message);
+
+//Q6
+//let whiteSpace="\n\t Mariam Aamir\t\n";
+//console.log(whiteSpace);
+
+//let withoutwhiteSpace=whiteSpace.trim();
+//console.log(withoutwhiteSpace);
+
+//Q7 Q8
+//console.log(4+4);
+//console.log(10-2);
+//console.log(2*4);
+//console.log(16/2);
+
+//Q9
+//let favouriteNumber=2
+//console.log("My favouriteNumber is" , favouriteNumber);
+
+//Q10
+//Date 3/17/2024
+
+//Q11
+//let friendName =["Mariam","Nimra","Mehwish","Rubab"]
+//console.log(friendName[0])
+//console.log(friendName[1])
+//console.log(friendName[2])
+//console.log(friendName[3])
+
+//Q12
+let friendName =["Mariam","Nimra","Mehwish","Rubab"]
+let message = "meet me at food junction sharp 6.pm"
+console.log(friendName[0],message)
+console.log(friendName[1],message)
+console.log(friendName[2],message)
+console.log(friendName[3],message)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
